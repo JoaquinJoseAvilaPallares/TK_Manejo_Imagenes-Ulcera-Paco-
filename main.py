e5cb7feb33cf930eae88ff7062630165a87b14ec
JoaquinAvila <jj-pallares@hotmail.com>
2024-12-15 19:41:21 +0100

    modificacion acceso archivo

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class Paco(tk.Tk):
         #self.iconbitmap('icono.ico')
         self.config(background='aqua')
         self.dias_semana = ['LUNES', 'MARTES', 'MIERCOLES', 'JUEVES', 'VIERNES', 'SABADO', 'DOMINGO']
-        self.imagenes_ulcera = os.listdir("C:\\Aplicaciones\\Manejo_imagenes_TK_(caso_ulcera_paco)\\Paco_Ulcera\\Fotos_Ulcera")
+        self.imagenes_ulcera = os.listdir("C://Users//Joaquin Avila//Documents//Proyectos//TK_Manejo_Imagenes(Ulcera-Paco)//Paco_Ulcera//Fotos_Ulcera")
         self.imagenes_pie_derecho_am = []
         self.imagenes_pie_derecho_pm = []
         self.imagenes_pie_izquierdo_am = []
@@ -30,7 +30,7 @@ class Paco(tk.Tk):
             etiqueta = tk.Label(self,text=self.dias_semana[dia]).grid(row=1, column=dia, pady=3)
 
         for dia in range(len(self.imagenes_pie_derecho_am)):
-            self.imagenes_am['imagen'+str(dia+1)] = PhotoImage(file=f'C:\\Aplicaciones\\Manejo_imagenes_TK_(caso_ulcera_paco)\\Paco_Ulcera\\Fotos_Ulcera\\{self.imagenes_pie_derecho_am[dia]}').subsample(6)
+            self.imagenes_am['imagen'+str(dia+1)] = PhotoImage(file=f'C://Users//Joaquin Avila//Documents//Proyectos//TK_Manejo_Imagenes(Ulcera-Paco)//Paco_Ulcera//Fotos_Ulcera//{self.imagenes_pie_derecho_am[dia]}').subsample(7)
             label = tk.Label(self, image=self.imagenes_am[f'imagen{dia+1}']).grid(row=2, column=dia, padx=3, pady=3)
 
         """Componentes para comparar las fotos de la ulcera tomadas por la noche"""
@@ -39,7 +39,7 @@ class Paco(tk.Tk):
             etiqueta = tk.Label(self, text=self.dias_semana[dia]).grid(row=4, column=dia, pady=3)
 
         for dia in range(len(self.imagenes_pie_derecho_pm)):
-            self.imagenes_pm['imagen' + str(dia + 1)] = PhotoImage(file=f'C:\\Aplicaciones\\Manejo_imagenes_TK_(caso_ulcera_paco)\\Paco_Ulcera\\Fotos_Ulcera\\{self.imagenes_pie_derecho_pm[dia]}').subsample(6)
+            self.imagenes_pm['imagen' + str(dia + 1)] = PhotoImage(file=f'C://Users//Joaquin Avila//Documents//Proyectos//TK_Manejo_Imagenes(Ulcera-Paco)//Paco_Ulcera//Fotos_Ulcera//{self.imagenes_pie_derecho_pm[dia]}').subsample(7)
             label = tk.Label(self, image=self.imagenes_pm[f'imagen{dia + 1}']).grid(row=5, column=dia, padx=3, pady=3)
 
     def filtro(self):
